397d3f2b576ff3880ba2581890197651779eb9b8
Robert P. <robert.v.panta@gmail.com>
2025-03-29 21:50:50 +0100

    update lab1 -comments

--- lab1/generate-keypair.ts
+++ lab1/generate-keypair.ts
@@ -1,8 +1,7 @@
 import { Keypair } from "@solana/web3.js";
 
-const keypair = Keypair.generate(); //API care creaza un KeyPair (cheie publica & cheie privata)
+const keypair = Keypair.generate(); // API that creates a KeyPair (public key & private key)
 
-console.log("✅ Keypair generated successfully!!");
-console.log("✅ PublicKey", keypair.publicKey.toBase58());
-console.log("✅ SecretKey", keypair.secretKey);
-
+console.log("🎉 Keypair generated successfully!!");
+console.log("🔑 PublicKey", keypair.publicKey.toBase58()); // Used to receive funds and for identification on the blockchain.
+console.log("⚠️ SecretKey", keypair.secretKey); // Used to sign transactions and must be kept secret.
