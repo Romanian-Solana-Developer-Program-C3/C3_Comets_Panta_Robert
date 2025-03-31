d642bcd8a6b2619d4e45cca2b9df52730a2c4474
Robert P. <robert.v.panta@gmail.com>
2025-03-31 22:38:42 +0200

    lab4

---
+++ lab4/rustlings/target/debug/deps/variables2-b146a4f0da33fe07.d
@@ -0,0 +1,9 @@
+/mnt/d/CometsOfWeb3/Project/C3_Comets_Panta_Robert/lab4/rustlings/target/debug/deps/libvariables2-b146a4f0da33fe07.rmeta: exercises/01_variables/variables2.rs Cargo.toml
+
+/mnt/d/CometsOfWeb3/Project/C3_Comets_Panta_Robert/lab4/rustlings/target/debug/deps/variables2-b146a4f0da33fe07.d: exercises/01_variables/variables2.rs Cargo.toml
+
+exercises/01_variables/variables2.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
