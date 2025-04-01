02910a765cdab67d74f76ba6dd4b18cbec57db35
Robert P. <robert.v.panta@gmail.com>
2025-04-01 12:55:16 +0200

    lab4

---
+++ lab4/rustlings/target/debug/deps/vecs1-deb31feb38e3e998.d
@@ -0,0 +1,5 @@
+/mnt/d/CometsOfWeb3/Project/C3_Comets_Panta_Robert/lab4/rustlings/target/debug/deps/vecs1-deb31feb38e3e998: exercises/05_vecs/vecs1.rs
+
+/mnt/d/CometsOfWeb3/Project/C3_Comets_Panta_Robert/lab4/rustlings/target/debug/deps/vecs1-deb31feb38e3e998.d: exercises/05_vecs/vecs1.rs
+
+exercises/05_vecs/vecs1.rs:
