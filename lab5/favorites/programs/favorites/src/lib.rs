47ad1d23d5fd50da493d1150db5f63643db9ee96
Robert P. <robert.v.panta@gmail.com>
2025-04-04 22:24:37 +0200

    lab5

--- lab5/favorites/programs/favorites/src/lib.rs
+++ lab5/favorites/programs/favorites/src/lib.rs
@@ -1,16 +1,72 @@
 use anchor_lang::prelude::*;
 
-declare_id!("FmK17VySbNkECs1AqdLZ1FrBG7QbnRk3PVPn6S3kpNej");
+declare_id!("FmK17VySbNkECs1AqdLZ1FrBG7QbnRk3PVPn6S3kpNej"); //Program address on the blockchain
 
 #[program]
 pub mod favorites {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        //msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn set_favorites(
+        ctx: Context<SetFavorites>, 
+        color:String, 
+        number:u64, 
+        hobbies: Vec<String>,
+    ) -> Result<()> {
+        msg!("Setting favorites");
+
+        let user_pubkey = ctx.accounts.user.key();
+
+        msg!("Program id: {}", ctx.program_id);
+        msg!(
+            "Setting {}'s favorite number to {} and color to {}",
+            user_pubkey,
+            number,
+            color
+    );
+        msg!("Hobbies: {:?}", hobbies);
+        ctx.accounts.favorites.set_inner(Favorites { 
+            color,
+            number,
+            hobbies
+        });
+
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+
+pub struct SetFavorites<'info>{
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"favorites", user.key().as_ref()],
+        space = 8 + Favorites::INIT_SPACE,
+        bump
+    )]
+    pub favorites: Account<'info, Favorites>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    #[max_len(50)]
+    pub color: String,
+    pub number: u64,
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>,
+}
+
+// Purpose: Create a 'Favorites' account where users can modify their favorite color, number, and hobbies
--- lab5/favorites/tests/favorites.ts
+++ lab5/favorites/tests/favorites.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Favorites } from "../target/types/favorites";
+import { publicKey } from "@coral-xyz/anchor/dist/cjs/utils";
 
 describe("favorites", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,22 @@ describe("favorites", () => {
 
   const program = anchor.workspace.Favorites as Program<Favorites>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  // it("Is initialized!", async () => {
+  //   // Add your test here.
+  //   const tx = await program.methods.initialize().rpc();
+  //   console.log("Your transaction signature", tx);
+  // });
+
+  it("Save a user's favorite", async () => {
+    const favoritesPdaAndBump = await publicKey.findProgramAddress(
+      [Buffer.from("favorites"), user.publicKey.toBuffer()],
+      program.programId
+    );
+
+    const favoritesPda = favoritesPdaAndBump[0];
+
+    const pdaState = await program.account.favorites.fetch(favoritesPda);
+
+  })
+
 });
