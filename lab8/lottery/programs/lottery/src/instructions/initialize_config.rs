5a978fb1b7f4ec53514601f502d1defdeca2541c
Robert P. <robert.v.panta@gmail.com>
2025-04-21 17:22:44 +0200

    Update initialize_config.rs

--- lab8/lottery/programs/lottery/src/instructions/initialize_config.rs
+++ lab8/lottery/programs/lottery/src/instructions/initialize_config.rs
@@ -1,10 +1,39 @@
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
+
 pub struct InitializeConfig{
-    
+    pub admin: Signer<'info>,
+
+    #[account(
+        init,
+        space = 8 + std::mem::size_of::<TokenLottery>(),
+        //seeds = [b"token_lottery".as_ref()],
+        bump,
+    )]
+    pub token_lottery: Account<'info, TokenLottery>,
+
+
+    pub system_program: Program<'info, System>,
+
 }
 
 pub fn handler(ctx: Context<InitializeConfig>) -> Result<()> {
     Ok(())
+}
+
+
+#[account]
+#[derive(InitSpace)]
+pub struct TokenLottery {
+    pub admin: Pubkey,
+    pub winner_chosen: bool,
+    pub bump: u8,
+    pub padding: [u8; 6],
+    pub ticket_price: u64,
+    pub reward_amount: u64,
+    pub tickets_number: u64,
+    pub start_time: u64,
+    pub end_time: u64,
+    
 }
